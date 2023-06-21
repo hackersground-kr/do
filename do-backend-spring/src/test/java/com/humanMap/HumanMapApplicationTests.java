af3b23a564e350649b27efe4fd90fb73d600f256
whitebear05 <woojs0505@naver.com>
2023-06-22 01:20:42 +0900

    Add spring backend

---
+++ do-backend-spring/src/test/java/com/humanMap/HumanMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humanMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

3626b20e99f868ca28bd58c37d56a995771992d1
wsi1212 <wsi1212@dgsw.hs.kr>
2023-06-23 00:57:59 +0900

    ⚡️ :: add random profile image

--- do-android/app/src/main/java/kr/hackersground/wsi/dodo/features/adapter/MemberAdapter.kt
+++ do-android/app/src/main/java/kr/hackersground/wsi/dodo/features/adapter/MemberAdapter.kt
@@ -1,5 +1,6 @@
 package kr.hackersground.wsi.dodo.features.adapter
 
+import com.bumptech.glide.Glide
 import kr.hackersground.wsi.dodo.R
 import kr.hackersground.wsi.dodo.base.BaseListAdapter
 import kr.hackersground.wsi.dodo.databinding.ItemMemberBinding
@@ -12,5 +13,10 @@ class MemberAdapter : BaseListAdapter<Member, ItemMemberBinding>(
 ) {
     override fun action(item: Member, binding: ItemMemberBinding) {
         binding.user = item
+        val randomNumber = (1..75).random()
+        Glide.with(binding.root.context)
+            .load("https://randomuser.me/api/portraits/men/${randomNumber}.jpg")
+            .centerCrop()
+            .into(binding.imProfile)
     }
 }

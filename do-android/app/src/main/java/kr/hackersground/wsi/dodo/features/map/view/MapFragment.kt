4e656c5fc176eead52f95b3c5a43c72e82bf7458
최민재 <cmjcmj1230@gmail.com>
2023-06-23 01:30:20 +0900

    ⚡️ :: add map recycler member view

--- do-android/app/src/main/java/kr/hackersground/wsi/dodo/features/map/view/MapFragment.kt
+++ do-android/app/src/main/java/kr/hackersground/wsi/dodo/features/map/view/MapFragment.kt
@@ -11,8 +11,6 @@ import androidx.lifecycle.lifecycleScope
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
-import com.naver.maps.map.overlay.Marker
-import com.naver.maps.map.util.MarkerIcons
 import dagger.hilt.android.AndroidEntryPoint
 import kr.hackersground.wsi.dodo.R
 import kr.hackersground.wsi.dodo.base.BaseFragment
